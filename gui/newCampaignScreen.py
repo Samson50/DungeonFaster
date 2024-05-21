f4daf11255879595464e7c615d38b1d3eae9a9a1
Jacob Weiss <jacob.t.weiss.mil@army.mil>
2024-05-21 11:59:36 -0400

    Solid start to new-campaign gui

--- gui/newCampaignScreen.py
+++ gui/newCampaignScreen.py
@@ -10,42 +10,68 @@ from kivy.uix.filechooser import FileChooserListView
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.popup import Popup
+from kivy.uix.scatter import Scatter
+from kivy.uix.switch import Switch
 from kivy.uix.textinput import TextInput
 
 from gui.menuManager import MenuManager
 
-
-class FloatInput(TextInput):
-
-    pat = re.compile("[^0-9]")
-
-    def insert_text(self, substring, from_undo=False):
-        pat = self.pat
-        if "." in self.text:
-            s = re.sub(pat, "", substring)
-        else:
-            s = ".".join(re.sub(pat, "", s) for s in substring.split(".", 1))
-        return super().insert_text(s, from_undo=from_undo)
+from model.map import Map
 
 
 class LabeledIntInput(BoxLayout):
-    def __init__(self, **kwargs):
+    def __init__(self, name, callback, delta, default, **kwargs):
         super().__init__(orientation="horizontal", **kwargs)
 
-        self.value: float = 0.0
+        self.value: float = default
+        self.update_value = callback
+        self.delta = delta
 
-        self.text = Label(text="Test", size_hint=(0.3, 1))
+        self.text = Label(text=name, size_hint=(0.3, 1))
         self.add_widget(self.text)
         self.input = FloatInput(
-            text=str(self.value), multiline=False, size_hint=(0.3, 1)
+            self, text=str(self.value), multiline=False, size_hint=(0.3, 1)
         )
         self.add_widget(self.input)
         dec_button = Button(text="<", size_hint=(0.2, 1))
-        # dec_button.bind(on_release=self.lower_value)
+        dec_button.bind(on_release=self.lower_value)
         self.add_widget(dec_button)
         inc_button = Button(text=">", size_hint=(0.2, 1))
-        # inc_button.bind(on_release=self.raise_value)
+        inc_button.bind(on_release=self.raise_value)
         self.add_widget(inc_button)
+
+    def raise_value(self, instance):
+        self.value += self.delta
+        self.input.text = str(self.value)
+        self.update_value(self.value)
+
+    def lower_value(self, instance):
+        self.value -= self.delta
+        self.input.text = str(self.value)
+        self.update_value(self.value)
+
+
+class FloatInput(TextInput):
+    # ^([0-9]+.?)?[0-9]*$
+    # pat = re.compile("^([0-9]+.?)?[0-9]*$")
+
+    def __init__(self, container: LabeledIntInput, **kwargs):
+        super().__init__(**kwargs)
+
+        self.container = container
+        self.pat = re.compile("[^0-9]")
+
+    def insert_text(self, substring, from_undo=False):
+        if "." in self.text:
+            s = re.sub(self.pat, "", substring)
+        else:
+            s = ".".join(re.sub(self.pat, "", s) for s in substring.split(".", 1))
+        super().insert_text(s, from_undo=from_undo)
+
+        self.container.value = float(self.text)
+        self.container.update_value(self.container.value)
+
+        return None
 
 
 class NewCampaignScreen(Screen):
@@ -55,6 +81,7 @@ class NewCampaignScreen(Screen):
 
         self.menuManager = manager
         self.fileChooser = None
+        self.map: Map = None
 
         layout = BoxLayout(orientation="vertical")
 
@@ -69,13 +96,8 @@ class NewCampaignScreen(Screen):
         editor_layout = BoxLayout(orientation="horizontal", size_hint=(1, 0.8))
         layout.add_widget(editor_layout)
 
-        controls_layout = BoxLayout(orientation="vertical", size_hint=(0.3, 1))
-        # offset: Label, TextInput, Button(<), Button(>)
-        controls_layout.add_widget(LabeledIntInput(size_hint=(1, 0.1)))
-        # box size: Label, TextInput, Button(<), Button(>)
-        controls_layout.add_widget(LabeledIntInput(size_hint=(1, 0.1)))
-        # hex-or-square: Switch
-        editor_layout.add_widget(controls_layout)
+        self.controls_layout = ControllerLayout(self)
+        editor_layout.add_widget(self.controls_layout)
 
         self.map_layout = BoxLayout(orientation="vertical", size_hint=(0.7, 1))
         getMapButton = Button(text="Select Overworld Map")
@@ -92,9 +114,12 @@ class NewCampaignScreen(Screen):
             size_hint=(1, 1),
             pos_hint={"center_x": 0.5, "center_y": 0.5},
         )
-        fileChooser = FileChooserListView(path=os.path.expanduser("~"))
+        fileChooser = FileChooserListView(
+            path="/home/minalan/repos/DungeonFaster/example/chult"
+        )
+        # fileChooser = FileChooserListView(path=os.path.expanduser("~"))
         self.fileChooser = fileChooser
-        fileChooser.bind(on_selection=self.selectOverworldMap)
+        # fileChooser.bind(on_selection=self.selectOverworldMap)
         buttonsLayout = BoxLayout(orientation="horizontal", size_hint=(1, 0.2))
         selectLayout.add_widget(fileChooser)
         selectLayout.add_widget(buttonsLayout)
@@ -111,9 +136,9 @@ class NewCampaignScreen(Screen):
         )
         self._popup.open()
 
-    def selectOverworldMap(self, instance: FileChooserListView):
-        self.overworldMapFile = instance.selection
-        print(self.overworldMapFile)
+    # def selectOverworldMap(self, instance: FileChooserListView):
+    #     self.overworldMapFile = instance.selection
+    #     print(self.overworldMapFile)
 
     def saveOverworldMap(self, instance):
         # Set overworld map file
@@ -127,17 +152,121 @@ class NewCampaignScreen(Screen):
         # Clear widgets from map_layout
         self.map_layout.clear_widgets()
 
-        # Create image from overworld map file
-        self.map_image = Image(
-            source=self.overworldMapFile,
-            width=self.map_layout.width,
-            height=self.map_layout.height,
-        )
-        # Add image to map layout
-
-        self.map_layout.add_widget(self.map_image)
-        # TODO: Add zoom in/out buttons
         self._popup.dismiss()
+
+        self.map = Map(self.overworldMapFile)
+
+        self.map.getZoomForSurface(self.map_layout)
+        self.map.draw(self.map_layout)
 
     def cancelPopup(self, instance):
         self._popup.dismiss()
+
+
+class ControllerLayout(BoxLayout):
+
+    def __init__(self, screen: NewCampaignScreen, **kwargs):
+        super().__init__(orientation="vertical", size_hint=(0.3, 1), **kwargs)
+
+        self.screen = screen
+
+        self.add_rocker_controller("Map Zoom", self.zoomIn, self.zoomOut)
+        self.add_rocker_controller("Scroll X", self.dummy_button, self.dummy_button)
+        self.add_rocker_controller("Scroll Y", self.dummy_button, self.dummy_button)
+
+        self.density_controller = LabeledIntInput(
+            "Box Size", self.setDensity, 0.5, 100, size_hint=(1, 0.08)
+        )
+        self.add_widget(self.density_controller)
+        self.xOffsetController = LabeledIntInput(
+            "X Offset", self.setXOffset, 1, 0, size_hint=(1, 0.08)
+        )
+        self.add_widget(self.xOffsetController)
+        self.yOffsetController = LabeledIntInput(
+            "Y Offset", self.setYOffset, 1, 0, size_hint=(1, 0.08)
+        )
+        self.add_widget(self.yOffsetController)
+
+        self.xMarginController = LabeledIntInput(
+            "X Margin", self.setXMargin, 1, 1, size_hint=(1, 0.08)
+        )
+        self.add_widget(self.xMarginController)
+        self.yMarginController = LabeledIntInput(
+            "Y Margin", self.setYMargin, 1, 1, size_hint=(1, 0.08)
+        )
+        self.add_widget(self.yMarginController)
+
+        # hex-or-square: Switch
+        hex_switch_layout = BoxLayout(orientation="horizontal", size_hint=(1, 0.08))
+        hex_switch_layout.add_widget(Label(text="Hex Grid"))
+        hex_switch_layout.add_widget(Switch())
+        self.add_widget(hex_switch_layout)
+
+        # Has hidden info: Switch
+        hidden_switch_layout = BoxLayout(orientation="horizontal", size_hint=(1, 0.08))
+        hidden_switch_layout.add_widget(Label(text="Has hidden areas"))
+        hidden_switch_layout.add_widget(Switch())
+        self.add_widget(hidden_switch_layout)
+
+        # Empty layout for spacing
+        self.add_widget(BoxLayout(size_hint=(1, 0.4)))
+
+    def dummy_button(self, value):
+        pass
+
+    def add_rocker_controller(self, name, on_plus, on_minus):
+        rocker_box = BoxLayout(orientation="horizontal", size_hint=(1, 0.08))
+        rocker_box.add_widget(Label(text=name))
+        plus_button = Button(text="+", size_hint=(0.3, 1))
+        plus_button.bind(on_release=on_plus)
+        rocker_box.add_widget(plus_button)
+        minus_button = Button(text="-", size_hint=(0.3, 1))
+        minus_button.bind(on_release=on_minus)
+        rocker_box.add_widget(minus_button)
+
+        self.add_widget(rocker_box)
+
+    def zoomIn(self, value: float):
+        if self.screen.map is None:
+            return
+        self.screen.map.zoom -= 1
+        self.screen.map.draw(self.screen.map_layout)
+
+    def zoomOut(self, value: float):
+        if self.screen.map is None:
+            return
+        self.screen.map.zoom += 1
+        self.screen.map.draw(self.screen.map_layout)
+
+    def setXOffset(self, value: float):
+        if self.screen.map is None:
+            return
+        self.screen.map.x_offset = value
+        self.screen.map.draw(self.screen.map_layout)
+
+    def setYOffset(self, value: float):
+        if self.screen.map is None:
+            return
+        self.screen.map.y_offset = value
+        self.screen.map.draw(self.screen.map_layout)
+
+    def setXMargin(self, value: float):
+        if self.screen.map is None:
+            return
+        self.screen.map.x_margin = value
+        self.screen.map.update_grid()
+        self.screen.map.draw(self.screen.map_layout)
+
+    def setYMargin(self, value: float):
+        if self.screen.map is None:
+            return
+        self.screen.map.y_margin = value
+        self.screen.map.update_grid()
+        self.screen.map.draw(self.screen.map_layout)
+
+    def setDensity(self, value: float):
+        if self.screen.map is None:
+            return
+        self.screen.map.pixel_density = value
+        self.screen.map.update_grid()
+        self.screen.map.draw(self.screen.map_layout)
---
+++ model/map.py
@@ -0,0 +1,202 @@
+from kivy.uix.image import Image
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+
+
+class Map:
+    def __init__(self, map_file: str = None):
+
+        self.map_file = map_file
+        self.image = Image(source=map_file)
+        self.tile: Image = Image(source="resources/map/highlight_grid.png")
+
+        (self.width, self.height) = self.image.texture.size
+
+        # How many box-width from the edges to leave without tiles
+        self.x_margin = 1
+        self.y_margin = 1
+
+        # Number of pixels to offset the first grid
+        self.x_offset = 0
+        self.y_offset = 0
+        self.zoom = 1
+
+        self.pixel_density = 60
+
+        # Number of grids on each axis we may need to display
+        self.grid_x = None
+        self.grid_y = None
+
+        self.grid_file = None
+
+        # Matrix of integers representing revealed/hidden grid tiles
+        self.grid_matrix = None
+        self.update_grid()
+
+        self.grid_image = None
+        self.highlight_image = None
+        self.scale_tiles()
+
+    def getZoomForSurface(self, surface: Widget):
+        if self.height / surface.height > self.width / surface.width:
+            self.zoom = self.width / surface.width
+        else:
+            self.zoom = self.height / surface.height
+
+    def getSubTexture(self, surface: Widget, x: int, y: int):
+        return self.image.texture.get_region(
+            x * self.zoom,
+            y * self.zoom,
+            surface.width * self.zoom,
+            surface.height * self.zoom,
+        )
+
+    def load(self, load_json):
+        self.map_file = load_json["map_file"]
+        self.x_offset = load_json["x_offset"]
+        self.y_offset = load_json["y_offset"]
+        self.x_margin = load_json["x_margin"]
+        self.y_margin = load_json["y_margin"]
+        self.pixel_density = load_json["pixel_density"]
+        self.zoom = load_json["zoom"]
+        self.grid_matrix = load_json["grid_matrix"]
+
+        self.update_zoom()
+
+    def save(self, save_data):
+        save_data["map_file"] = self.map_file
+        save_data["x_offset"] = self.x_offset
+        save_data["y_offset"] = self.y_offset
+        save_data["x_margin"] = self.x_margin
+        save_data["y_margin"] = self.y_margin
+        save_data["pixel_density"] = self.pixel_density
+        save_data["zoom"] = self.zoom
+        save_data["grid_matrix"] = self.grid_matrix
+
+    def update_grid(self):
+        """
+        Calculate the number of tiles available on the x and y axis from the size of
+        the background image and the current width of each tile
+        """
+
+        self.grid_x = int(self.width / self.pixel_density - 2 * self.x_margin)
+        self.grid_y = int(self.height / self.pixel_density - 2 * self.y_margin)
+        self.grid_matrix = [[0] * self.grid_y] * self.grid_x
+
+    def scale_tiles(self):
+        self.tile.size = (
+            self.pixel_density / self.zoom,
+            self.pixel_density / self.zoom,
+        )
+
+    def draw_grid(self, widget: Widget, x, y):
+        grid_x, grid_y = self.index_to_pixel(x, y)
+        with widget.canvas:
+            Image(source="resources/map/highlight_grid.png", pos=(grid_x, grid_y))
+
+    def draw_grids(self, surface):
+        for x in range(self.grid_x):
+            for y in range(self.grid_y):
+                # if self.grid_matrix[y][x] == 1:
+                self.draw_grid(surface, x, y)
+
+    def highlightAtIndex(self, surface: Widget, i: int, j: int):
+        self.tileAtIndex(surface, "resources/map/highlight_grid.png", i, j)
+
+    def tileAtIndex(self, surface: Widget, source: str, i: int, j: int):
+        Image(
+            source=source,
+            size=(
+                self.pixel_density / self.zoom,
+                self.pixel_density / self.zoom,
+            ),
+            pos=(
+                surface.pos[0]
+                + (self.x_offset + (self.x_margin + i) * self.pixel_density)
+                / self.zoom,
+                surface.pos[1]
+                + (self.y_offset + (self.y_margin + j) * self.pixel_density)
+                / self.zoom,
+            ),
+        )
+
+    def sparseTiles(self, surface: Widget, interval: int):
+        for i in range(self.grid_x):
+            if i % interval == 0 or i == self.grid_x - 1:
+                for j in range(self.grid_y):
+                    if j % interval == 0 or j == self.grid_y - 1:
+                        self.highlightAtIndex(surface, i, j)
+
+    # draw(self, surface: Widget, x: int, y: int)
+
+    # TODO: Temporary for initial testing
+    def draw(self, surface: Widget):
+        sub_texture = self.getSubTexture(surface, 0, 0)
+
+        surface.canvas.clear()
+        with surface.canvas:
+            Rectangle(
+                texture=sub_texture,
+                size=(surface.width, surface.height),
+                pos=surface.pos,
+            )
+            self.sparseTiles(surface, 5)
+
+    def flip_grid(self, x: int, y: int) -> None:
+        """Invert the value of the grid tile at position x, y
+
+        Args:
+            x (int): Target tile x coordinate
+            y (int): Target tile y coordinate
+        """
+        selected_grid = self.get_grid(x, y)
+        print(selected_grid)
+
+        if not selected_grid:
+            return
+
+        matrix_grid = self.grid_matrix[selected_grid[1]][selected_grid[0]]
+
+        if matrix_grid == 0:
+            self.grid_matrix[selected_grid[1]][selected_grid[0]] = 1
+        else:
+            self.grid_matrix[selected_grid[1]][selected_grid[0]] = 0
+
+    def index_to_pixel(self, x: int, y: int) -> tuple[int, int]:
+        """Get the x and y pixel offsets relative to display surface of the tile at index position (x, y)
+
+        Args:
+            x (int): x coordinate of tile
+            y (int): y coordinate of tile
+
+        Returns:
+            tuple[int, int]: Position of the tile in x, y pixels relative to display surface
+        """
+        px = (self.x_offset + (self.x_margin + x) * self.pixel_density) / self.zoom
+        py = (self.y_offset + (self.y_margin + y) * self.pixel_density) / self.zoom
+
+        return px, py
+
+    def get_grid(self, x, y):
+        pass
+
+    def update_zoom(self):
+        pass
+
+    def shift_right(self):
+        pass
+
+    def shift_left(self):
+        pass
+
+    def shift_up(self):
+        pass
+
+    def shift_down(self):
+        pass
+
+    def zoom_in(self):
+        pass
+
+    def zoom_out(self):
+        pass
